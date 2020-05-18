48c4efd96dbe6959b0f30f8565f50b19d4c6a826
cglewis <clewis@iqt.org>
2020-05-18 11:33:17 -0700

    break out scripts

---
+++ openvswitch/scripts/add_port.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# TODO handle when more than one?
+BR_NAME=$(ovs-vsctl list-br)
+
+ovs-vsctl add-port $BR_NAME $1
--- openvswitch/scripts/del_port.sh
+++ openvswitch/scripts/del_port.sh
@@ -3,5 +3,4 @@
 # TODO handle when more than one?
 BR_NAME=$(ovs-vsctl list-br)
 
-ovs-vsctl add-port $BR_NAME $1
-ovs-vsctl set-controller $BR_NAME $2 $3
+ovs-vsctl del-port $BR_NAME $1
