e38c4dd26bb67102e63087fef45e4463f01a2ff2
Josh Bailey <josh@vandervecken.com>
2020-05-28 12:03:53 +1200

    Don't dodge DNS.

--- test_dovesnap.sh
+++ test_dovesnap.sh
@@ -32,7 +32,7 @@ done
 docker-compose build && docker-compose up -d || exit 1
 docker network create testnet -d ovs -o ovs.bridge.mode=nat -o ovs.bridge.dpid=0x1 -o ovs.bridge.controller=tcp:127.0.0.1:6653 || exit 1
 # github test runner can't use ping.
-docker run -t --net=testnet --dns 4.2.2.2 --rm busybox wget -q -O- bing.com || exit 1
+docker run -t --net=testnet --rm busybox wget -q -O- bing.com || exit 1
 docker network rm testnet || exit 1
 docker-compose stop
 
