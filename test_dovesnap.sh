5864cb054798d78de06b8b38922a271f7cadb23b
Josh Bailey <josh@vandervecken.com>
2020-07-09 17:06:42 +1200

    Move to all faucet/confrpc.

--- test_dovesnap.sh
+++ test_dovesnap.sh
@@ -2,8 +2,6 @@
 
 export TMPDIR=/tmp
 export FAUCET_CONFIG=$TMPDIR/faucet.yaml
-export FAUCET_LOG=$TMPDIR/faucet.log
-export FAUCET_EXCEPTION_LOG=$TMPDIR/faucet_exception.log
 
 cat >$FAUCET_CONFIG <<EOC
 dps:
@@ -19,22 +17,17 @@ dps:
             native_vlan: 100
 EOC
 
-/usr/local/bin/faucet --version || exit 1
-nohup /usr/local/bin/faucet &
-FAUCETPID=$!
-
-while true ; do
-	wget -q -O/dev/null localhost:9302
-	if [ $? -eq 0 ] ; then break ; fi
-	echo waiting for faucet...
-	sleep 1
-done
-
-docker-compose build && docker-compose up -d || exit 1
+mkdir -p /opt/dovesnap || exit 1
+docker-compose build && FAUCET_CONFIG_DIR=$TMPDIR docker-compose -f docker-compose.yml -f docker-compose-standalone.yml up -d || exit 1
+wget --retry-connrefused --tries=20 -q -O/dev/null localhost:9302 > /dev/null || exit 1
 docker network create testnet -d ovs -o ovs.bridge.mode=nat -o ovs.bridge.dpid=0x1 -o ovs.bridge.controller=tcp:127.0.0.1:6653 || exit 1
+DPSTATUS=""
+while [ "$DPSTATUS" == "" ] ; do
+	sleep 1
+	DPSTATUS=$(wget -q -O- localhost:9302|grep -E "^dp_status"|grep -E "1.0$")
+	echo $DPSTATUS
+done
 # github test runner can't use ping.
 docker run -t --net=testnet --rm busybox wget -q -O- bing.com || exit 1
 docker network rm testnet || exit 1
-docker-compose stop
-
-kill $FAUCETPID
+FAUCET_CONFIG_DIR=$TMPDIR docker-compose -f docker-compose.yml -f docker-compose-standalone.yml stop
