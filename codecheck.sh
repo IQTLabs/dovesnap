0517cab4cfca623cda225fd2b064368af1da4291
Josh Bailey <josh@vandervecken.com>
2020-07-14 14:18:52 +1200

    Make gofmt chattier.

--- codecheck.sh
+++ codecheck.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
+echo running gofmt...
 GOFMTOUT=$(gofmt -l *go */*go)
-
 if [ "$GOFMTOUT" != "" ] ; then
-	echo gofmt reports files with formatting inconsistencies: $GOFMTOUT
+	echo FAIL: gofmt reports files with formatting inconsistencies: $GOFMTOUT
 	exit 1
 fi
 
+echo ok
 exit 0
