69a8d7019c7c8da85336233afe216f84a956d62f
Josh Bailey <josh@vandervecken.com>
2020-08-28 15:11:35 +1200

    resolve non container bridges.

--- graph_dovesnap/graph_dovesnap.py
+++ graph_dovesnap/graph_dovesnap.py
@@ -19,6 +19,7 @@ class GraphDovesnap:
     OFP_LOCAL = 4294967294
     DOCKER_URL = 'unix://var/run/docker.sock'
     OUTPUT_FILE = 'dovesnapviz'
+    PATCH_PREFIX = 'ovp'
 
     def __init__(self):
         return
@@ -78,11 +79,9 @@ class GraphDovesnap:
             return match[1]
         return None
 
-    def _scrape_bridge_interfaces(self, network):
-        network_id = network['Id']
-        bridge_name = self._dovesnap_bridgename(network_id)
+    def _scrape_bridge_ports(self, bridgename):
         lines = self._scrape_container_cmd(
-            self.OVS_NAME, ['ovs-ofctl', 'dump-ports-desc', bridge_name])
+            self.OVS_NAME, ['ovs-ofctl', 'dump-ports-desc', bridgename])
         matching_lines = self._get_matching_lines(
             lines, r'^\s*(\d+|LOCAL)\((\S+)\).+$')
         port_desc = {}
@@ -94,6 +93,17 @@ class GraphDovesnap:
             port = int(port)
             port_desc[desc] = port
         return port_desc
+
+    def _scrape_all_bridge_ports(self):
+        all_port_desc = {}
+        lines = self._scrape_container_cmd(
+            self.OVS_NAME, ['ovs-vsctl', 'list-br'])
+        matching_lines = self._get_matching_lines(
+            lines, r'^(\S+)$')
+        for match in matching_lines:
+            bridgename = match[0]
+            all_port_desc[bridgename] = self._scrape_bridge_ports(bridgename)
+        return all_port_desc
 
     def _scrape_container_veths(self):
         container_veths = {}
@@ -108,6 +118,21 @@ class GraphDovesnap:
             mac = match[3]
             container_veths[iflink] = (ifname, mac)
         return container_veths
+
+    def _scrape_patch_veths(self):
+        patch_veths = {}
+        process = subprocess.Popen(
+            ['ip', '-o', 'link', 'show', 'type', 'veth'], stdout=subprocess.PIPE)
+        matching_lines = self._get_matching_lines(
+            process.stdout.readlines(),
+            r'^\d+:\s+(%s[^\@]+)\@([^\:\s]+).+link\/ether\s+(\S+).+$' % self.PATCH_PREFIX)
+        for match in matching_lines:
+            ifname = match[1]
+            peerifname = match[2]
+            mac = match[3]
+            patch_veths[ifname] = (peerifname, mac)
+        assert len(patch_veths) % 2 == 0
+        return patch_veths
 
     def _get_network_mode(self, network):
         return network['Options'].get('ovs.bridge.mode', 'flat')
@@ -125,16 +150,19 @@ class GraphDovesnap:
             raise GraphDovesnapException('cannot find dovesnap container')
         networks = self._get_dovesnap_networks(client)
         container_veths = self._scrape_container_veths()
-        all_port_desc = {}
+        patch_veths = self._scrape_patch_veths()
+        all_port_desc = self._scrape_all_bridge_ports()
+        unresolved_links = []
+        network_id_name = {}
         for network in networks:
             network_id = network['Id']
             network_name = network['Name']
             network = client.inspect_network(network_id)
-            bridge_name = self._dovesnap_bridgename(network_id)
-            all_port_desc[network_id] = self._scrape_bridge_interfaces(network)
+            bridgename = self._dovesnap_bridgename(network_id)
             options = ['%s: %s' % (option.split('.')[-1], optionval)
                 for option, optionval in network['Options'].items()]
-            network_label = '\n'.join([network_name, bridge_name] + options)
+            network_label = '\n'.join([network_name, bridgename] + options)
+            network_id_name[bridgename] = network_id
             dot.node(network_id, network_label)
             container_ports = set()
             for container_id, container in network['Containers'].items():
@@ -150,24 +178,46 @@ class GraphDovesnap:
                         if ip:
                             host_label.append(ip)
                         host_label.extend(labels)
-                        ofport = all_port_desc[network_id][br_ifname]
+                        ofport = all_port_desc[bridgename][br_ifname]
                         container_ports.add(ofport)
                         edge_label = '%u' % ofport
                         dot.node(container_id, '\n'.join(host_label))
                         dot.edge(network_id, container_id, edge_label)
                         break
             mode = self._get_network_mode(network)
-            lbport = self._get_lb_port(network)
-            for br_desc, ofport in all_port_desc[network_id].items():
+            for br_desc, ofport in all_port_desc[bridgename].items():
                 if ofport in container_ports:
                     continue
                 if ofport == self.OFP_LOCAL:
                     if mode == 'nat':
                         dot.edge(network_id, 'NAT')
-                elif ofport == lbport:
-                    dot.edge(network_id, br_desc, 'mirror: %u' % lbport)
+                elif br_desc in patch_veths:
+                    unresolved_links.append((bridgename, br_desc, ofport))
                 else:
                     dot.edge(network_id, br_desc, str(ofport))
+
+        non_container_bridges = set()
+
+        # wire up links to non container bridges.
+        for bridgename, br_desc, ofport in unresolved_links:
+            network_id = network_id_name[bridgename]
+            peer_br_desc = patch_veths[br_desc][0]
+            for peer_bridgename, port_desc in all_port_desc.items():
+                if peer_br_desc in port_desc:
+                    peer_ofport = port_desc[peer_br_desc]
+                    dot.edge(network_id, peer_bridgename, '%u : %u' % (ofport, peer_ofport))
+                    if peer_bridgename not in network_id_name:
+                        non_container_bridges.add(peer_bridgename)
+                    break
+
+        # resolve any remaining ports, on non container bridges.
+        for bridgename in non_container_bridges:
+            for br_desc, ofport in all_port_desc[bridgename].items():
+                if ofport == self.OFP_LOCAL:
+                    continue
+                if br_desc not in patch_veths:
+                    dot.edge(bridgename, br_desc, str(ofport))
+
         dot.format = 'png'
         dot.render(self.OUTPUT_FILE)
         # leave only PNG
