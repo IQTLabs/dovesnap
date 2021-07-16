a38e65a99b927a094ccd1496816ba71788e6caf0
cglewis <clewis@iqt.org>
2021-07-16 14:03:04 -0700

    need libperl to compile graphviz

--- tests/install_test.sh
+++ tests/install_test.sh
@@ -9,7 +9,7 @@ sudo modprobe openvswitch && \
   sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common && \
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && \
   sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" && \
-  sudo apt-get update && sudo apt-get install docker-ce docker-ce-cli containerd.io wget python3-setuptools python3-dev udhcpd jq && \
+  sudo apt-get update && sudo apt-get install docker-ce docker-ce-cli containerd.io libperl-dev wget python3-setuptools python3-dev udhcpd jq && \
   curl -fsSLO https://gitlab.com/api/v4/projects/4207231/packages/generic/graphviz-releases/2.47.3/graphviz-2.47.3.tar.gz && \
   tar -xf graphviz-2.47.3.tar.gz && cd graphviz-2.47.3 && ./configure && make && sudo make install && \
   sudo pip3 install -r graph_dovesnap/requirements.txt
