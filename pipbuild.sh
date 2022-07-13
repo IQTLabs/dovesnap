b08a26ad018574b075acc97e0c5407068ba01e47
Ryan <rashley@iqt.org>
2022-07-13 09:09:30 -0400

    update pip/setuptools before running install

--- pipbuild.sh
+++ pipbuild.sh
@@ -2,4 +2,5 @@
 
 sudo apt-get update && \
   sudo apt-get install -yq --no-install-recommends python3 python3-dev python3-setuptools && \
+  python3 -m pip install -U pip && \
   python3 -m pip install .
