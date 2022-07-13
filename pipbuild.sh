3c7acf3764a202cd4f46fc1ad834a03a26f0a5e0
Josh Bailey <josh@vandervecken.com>
2022-07-13 20:46:52 +0000

    need setuptools upgrade + sdist build for pypi.

--- pipbuild.sh
+++ pipbuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo apt-get update && \
-  sudo apt-get install -yq --no-install-recommends python3 python3-dev python3-setuptools && \
-  python3 -m pip install -U pip && \
+  sudo apt-get install -yq --no-install-recommends python3 python3-dev && \
+  python3 -m pip install -U pip setuptools && \
   python3 -m pip install .
