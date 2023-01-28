58810ddc35cc9ebb437a993b480d871356e929c4
Josh Bailey <josh@vandervecken.com>
2023-01-28 15:36:56 +0000

    upgrade pip.

--- poetrybuild.sh
+++ poetrybuild.sh
@@ -2,6 +2,8 @@
 
 sudo apt-get update && \
   sudo apt-get install -yq --no-install-recommends python3 python3-dev && \
+  sudo apt-get remove python3-yaml python3-pexpect && \
+  sudo pip3 install -U pip && \
   sudo pip3 install poetry==1.1.15 && \
   poetry config virtualenvs.create false && \
   sudo poetry install
