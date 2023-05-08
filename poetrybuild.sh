574d4513f91f3f2683dc814f4c22a790f0e07dc1
Josh Bailey <josh@vandervecken.com>
2023-05-08 16:03:33 +1200

    2nd hotfix.

--- poetrybuild.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo apt-get update && \
-  sudo apt-get remove python3-yaml python3-pexpect && \
-  sudo pip3 install -U pip && \
-  poetry config virtualenvs.create false && \
-  sudo poetry install
