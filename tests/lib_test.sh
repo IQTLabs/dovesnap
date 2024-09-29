2c295dd63f0727c5861b7fd4f1745513d12d167e
Josh Bailey <josh@vandervecken.com>
2024-09-29 08:14:06 +0000

    logs.

--- tests/lib_test.sh
+++ tests/lib_test.sh
@@ -164,7 +164,7 @@ wait_faucet ()
                 OUTPUT=$(docker ps -q --filter health=healthy --filter name=faucetconfrpc)
                 sleep 1
         done
-        docker logs dovesnap-plugin
+        docker logs dovesnap-plugin-1
 }
 
 wait_acl ()
