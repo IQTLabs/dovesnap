75f460d1fec71fb46c834b96e3d434d0e7805544
Josh Bailey <anarkiwi@users.noreply.github.com>
2025-11-29 08:23:40 +1300

    Fix installation script for Docker and dependencies

--- tests/install_test.sh
+++ tests/install_test.sh
@@ -9,7 +9,6 @@ sudo modprobe openvswitch && \
   sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common && \
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && \
   sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" && \
-  sudo apt-get update && sudo apt-get install docker-ce docker-ce-cli containerd.io && \
   sudo apt-get install graphviz wget udhcpd jq nftables && sudo nft flush ruleset && sudo apt-get purge nftables && sudo apt-get --reinstall install iptables && \
   sudo update-alternatives --set iptables /usr/sbin/iptables-legacy && \
   sudo apt-get update && sudo apt-get install docker-ce docker-ce-cli containerd.io && \
