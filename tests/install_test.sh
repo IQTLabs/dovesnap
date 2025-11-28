b3471890d700db5a8bbd740c26b11ebdde6910ea
Josh Bailey <josh@vandervecken.com>
2025-11-28 19:16:43 +0000

    tables.

--- tests/install_test.sh
+++ tests/install_test.sh
@@ -9,9 +9,9 @@ sudo modprobe openvswitch && \
   sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common && \
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && \
   sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" && \
-  sudo apt-get update && sudo apt-get install docker-ce docker-ce-cli containerd.io && \
   sudo apt-get install graphviz wget udhcpd jq nftables && sudo nft flush ruleset && sudo apt-get purge nftables && sudo apt-get --reinstall install iptables && \
   sudo update-alternatives --set iptables /usr/sbin/iptables-legacy && \
+  sudo apt-get update && sudo apt-get install docker-ce docker-ce-cli containerd.io && \
   sudo systemctl restart docker && \
   cd openvswitch && docker build -f Dockerfile . -t iqtlabs/openvswitch:v3.2.1 && cd .. && \
   sudo ip link && sudo ip addr
